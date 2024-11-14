6efcd75452ad1ab6af4ab6e11c5a77f9b4170856
Tat Chan <tatchan@berkeley.edu>
2024-11-14 05:27:03 -0800

    update for projects listings

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,14 @@
 export const LINKS = {
-  github: "https://github.com/tchan102",
+  github: "https://github.com/tchan102/",
   linkedin: "https://www.linkedin.com/in/tatchungchan/",
-  calhack: "https://www.linkedin.com/posts/tatchungchan_what-an-incredible-weekend-at-calhacks-110-activity-7255148156189839361-JuiZ",
-  paper: "https://link.springer.com/article/10.1007/s11538-024-01307-y",
+  calhack: "https://www.linkedin.com/posts/tatchungchan_what-an-incredible-weekend-at-calhacks-110-activity-7255148156189839361-JuiZ/",
+  paper: "https://link.springer.com/article/10.1007/s11538-024-01307-y/",
+  proj1: "https://wms-demo-ten.vercel.app/",
+  proj2_git: "https://github.com/tchan102/svd-compression/",
+  proj2_demo: "http://svdloadbalancer-641359121.us-west-1.elb.amazonaws.com/",
+  proj3: "https://github.com/ZethT/teamcook/",
+  proj4: "https://yt-web-client-2asppecb5q-uc.a.run.app/",
+
 };
 
 export const loaderAnimation = [
